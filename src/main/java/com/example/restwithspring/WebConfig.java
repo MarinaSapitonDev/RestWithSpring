aa0e24cea4e75e810db7de70b80cdc2e1e3d41d8
m.sapiton <m.sapiton@godeltech.com>
2023-02-03 15:26:28 +0100

    Add spring configs, not working test

---
+++ src/main/java/com/example/restwithspring/WebConfig.java
@@ -0,0 +1,11 @@
+package com.example.restwithspring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.example.restwithspring")
+public class WebConfig {
+}
